8dc2e5e68b9d1d6fa14b0789d73e9dd1966236ca
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-11-08 00:07:11 -0600

    added relations on my graphql objects

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema'); //allows the express package to understand graphql and will act as a middleware
+const cors = require('cors');
 
 const server = express();
 
-server.use('/graphql', graphqlHTTP({ schema })); //put a schema on the graphqlHTTP middleware!!
+server.use(cors());
+server.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+); //put a schema on the graphqlHTTP middleware!!
 
-server.listen(9000, () => {
+server.listen(8000, () => {
   console.log('\n === API on port 9K === \n');
 });
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,21 +1,60 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql; //destructurized on the graphql object
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql; //destructurized on the graphql object
 
 let books = [
   {
-    name: 'Name',
+    name: 'Name 1',
     genre: 'the genre',
     id: '1'
+  },
+  {
+    name: 'Name 2',
+    genre: 'the genre 2',
+    id: '2'
   }
+];
+
+let authors = [
+  {
+    name: 'Carlo Clamucha',
+    age: 21,
+    id: '1',
+    authorId: '1'
+  },
+  { name: 'Kevin Brack', age: 22, id: '2', authorId: '2' }
 ];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        console.log(parent);
+        return _.find(authors, { authorId: parent.id });
+      }
+    }
+  })
+}); // needed for defining a graph and how it will look.
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    authorId: { type: GraphQLID }
   })
 }); // needed for defining a graph and how it will look.
 
@@ -24,10 +63,18 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //args.id
         //code to get data
+        return _.find(books, { id: args.id });
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
       }
     }
   }
