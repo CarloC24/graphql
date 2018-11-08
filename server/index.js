3c03bdc6496d79023c9eb4fb9ba6eeb65918fa3d
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-11-07 22:08:47 -0600

    added schema now working on the resolve

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql'); //allows the express package to understand graphql and will act as a middleware
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema'); //allows the express package to understand graphql and will act as a middleware
 
 const server = express();
 
-server.use('/graphql', graphqlHTTP({}));//put a schema on the graphqlHTTP middleware!!
+server.use('/graphql', graphqlHTTP({ schema })); //put a schema on the graphqlHTTP middleware!!
 
 server.listen(9000, () => {
   console.log('\n === API on port 9K === \n');
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql; //destructurized on the graphql object
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql; //destructurized on the graphql object
+
+let books = [
+  {
+    name: 'Name',
+    genre: 'the genre',
+    id: '1'
+  }
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -10,3 +18,21 @@ const BookType = new GraphQLObjectType({
     genre: { type: GraphQLString }
   })
 }); // needed for defining a graph and how it will look.
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //args.id
+        //code to get data
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
